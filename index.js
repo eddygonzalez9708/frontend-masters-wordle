9472aed1008cb78879b6abfdc5fb68f424c2823a
eddygonzalez9708 <edward.gonzalez9708@gmail.com>
2024-06-03 12:05:30 -0400

    Completed game logic.

--- index.js
+++ index.js
@@ -3,14 +3,12 @@ let square = 0
 let word = ""
 let correctWord = ""
 
-function validKey(key) {
-    let test = /^[a-zA-z]$/.test(key)
-
-    if (test) {
-        addKeyToSquare(key)
-    } else if (key === "Enter") {
+function validKey(e) {
+    if (/^[a-zA-z]$/.test(e.key)) {
+        addKeyToSquare(e.key)
+    } else if (e.key === "Enter") {
         validateRow()
-    } else if (key === "Backspace") {
+    } else if (e.key === "Backspace") {
         deleteSquareKey()
     }
 }
@@ -19,17 +17,21 @@ async function validateRow() {
     if (word.length === 5) {
         const isWord = await validateWord()
         if (isWord) {
-            isCorrectWord()
-        } else {
+            if (word === correctWord) {
+                win()
+            } else {
+                submitRow()
+            }
+        } else if (isWord === false) {
             let children = getChildren()
 
             Array.from(children).forEach(child => {
-                child.classList.add("error")
+                child.classList.add("invalid-word")
             });
 
             setTimeout(() => {
                 Array.from(children).forEach(child => {
-                    child.classList.remove("error")
+                    child.classList.remove("invalid-word")
                 });
             }, 1000)
         }
@@ -45,30 +47,83 @@ async function validateWord() {
     if (response.status >= 200 && response.status <= 300) {
         const payload = await response.json()
         return payload.validWord
+    } else {
+        alert("The game cannot be played because the word submitted could not be validated.")
+        console.error(response.statusText)
     }
 }
 
 function addKeyToSquare(key) {
     let children = getChildren()
-
     children[square].innerText = key.toUpperCase()
 
-    if (square !== 4) {
+    if (square === 4) {
+        word = word.substring(0, 4) + key
+    } else {
         square += 1
         word += key
-
-    } else {
-        word = word.substring(0, 4) + key
     }
 }
 
 function deleteSquareKey() {
     let children = getChildren()
-    children[square].innerText = ""
 
-    if (square !== 0) {
+    if (square !== 0 && !children[square].innerText) {
         square -= 1
-        word = word.slice(0, word.length - 1)
+    } 
+
+    children[square].innerText = ""
+    word = word.slice(0, word.length - 1)
+}
+
+function win() {
+    let children = getChildren()
+    let title = document.querySelector(".title")
+    document.querySelector("html").removeEventListener("keyup", validKey)
+
+    alert("You win!")
+
+    Array.from(children).forEach(child => {
+        child.style.backgroundColor = "green"
+        child.style.color = "white"
+    })
+
+    title.classList.add("rainbow")
+}
+
+function lose() {
+    alert(`You lose, the word was ${correctWord}`)
+    document.querySelector("html").removeEventListener("keyup", validKey)
+}
+
+function submitRow() {
+    let children = getChildren()
+    let cache = {}
+
+    Array.from(children).forEach((child, index) => {
+        let char = child.innerText.toLowerCase()
+        child.style.color = "white"
+
+        if (char === correctWord[index]) {
+            child.style.backgroundColor = "green"
+        } else {
+            const search = correctWord.indexOf(char, cache[char])
+
+            if (search > -1) {
+                child.style.backgroundColor = "yellow"
+                cache[char] + search + 1
+            } else {
+                child.style.backgroundColor = "grey"
+            }
+        }
+    })
+
+    if (row === 6) {
+        lose()
+    } else {
+        row += 1
+        square = 0
+        word = ""
     }
 }
 
@@ -84,9 +139,7 @@ async function init() {
         correctWord = payload.word
         document
             .querySelector("html")
-            .addEventListener("keyup", (event) => {
-                validKey(event.key)
-            })
+            .addEventListener("keyup", validKey)
     } else {
         alert("The game cannot be played because a random word could not be generated.")
         console.error(response.statusText)
